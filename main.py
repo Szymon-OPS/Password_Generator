4363758c453fef72b8d8f40fc312107c70ec1622
Szymon Szczepiński <75580477+Szymon-OPS@users.noreply.github.com>
2021-09-06 00:47:12 +0200

    Added password generator
    -password generator function with automatic copying of the password
    -function assigned to a button

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from tkinter import *
+from random import choice, randint, shuffle
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR ----------------------
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    # LIST Comprehension version with methods imported from random module
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+
+    shuffle(password_list)
+
+    # Merge all password characters using join() method
+    password = "".join(password_list)
+    password_entry.insert(0, string=password)
+    # Automatically copy generated password using pyperclip package
+    pyperclip.copy(password)
 
 # ---------------------------- UI SETUP --------------------------------
 window = Tk()
@@ -45,8 +68,8 @@ password_entry.grid(column=1, row=3)
 
 # BUTTONS
 
-## Generate Password - will generate password when pressed
-generate_button = Button(text="Generate Password")
+## Calls generate_password() when pressed
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
 
 ## Add - will save password when pressed
